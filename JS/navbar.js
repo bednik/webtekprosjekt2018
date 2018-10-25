6453fd4bf51f618b4253a61855a675437d898133
Bendik <bendikbrunvoll@gmail.com>
2018-10-25 12:41:45 +0200

    sfed

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,3 +1,9 @@
+// Toggle-funksjon for
+function toggleNavigation() {
+
+}
+
+// Importer navbaren. Skriptet plasseres der man vil at navbaren skal være.
 var content = document.querySelector('link[rel="import"]').import;
 var el = content.querySelector('.navbar');
 document.body.appendChild(el.cloneNode(true));
