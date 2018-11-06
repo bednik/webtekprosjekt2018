9928ae2dba974f03642bea22ea7f026fba3abc98
unknown <Runar Sther>
2018-11-06 18:25:02 +0100

    Ryddet opp i main.css og filstruktur

--- JS/footer.js
+++ JS/footer.js
@@ -1,4 +1,4 @@
 // Importer navbaren. Skriptet plasseres der man vil at navbaren skal være.
-let content = document.querySelector('link[rel="import"]').import;
-let el = content.querySelector('.footer');
+var content = document.querySelector('link[rel="import"]').import;
+var el = content.querySelector('.footer');
 document.body.appendChild(el.cloneNode(true));
