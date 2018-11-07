9b96c1d918bac42e616aac6c870f74e72aae3e5a
SanderHestvik <sander_hestvik@hotmail.com>
2018-11-07 11:31:46 +0100

    fixed javascript parsing of navbar and footer in all the pages. Changed contact info in footer

--- JS/footer.js
+++ JS/footer.js
@@ -1,4 +1,10 @@
 // Importer navbaren. Skriptet plasseres der man vil at navbaren skal være.
-var content = document.querySelector('link[rel="import"]').import;
-var el = content.querySelector('.footer');
-document.body.appendChild(el.cloneNode(true));
+let footervar = "<footer id=\"footer\">\n" +
+    "    <div>\n" +
+    "        <div id=\"copyright\">&copy; 2018 Diavola Calabrese</div>\n" +
+    "        <div id=\"footer_contact\">oetker@oetker.no |  668 23 420</div>\n" +
+    "    </div>\n" +
+    "</footer>\n";
+
+let element = document.getElementById("footer");
+element.innerHTML = footervar
