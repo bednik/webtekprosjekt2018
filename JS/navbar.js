4a95ca48e57ba5f4f94d48af59394b2419e8b462
ingvlt <ilthon@hotmail.no>
2018-11-07 11:58:06 +0100

    oppdatering

--- JS/navbar.js
+++ JS/navbar.js
@@ -6,6 +6,30 @@ let el = content.querySelector('.navbar');
 let link = document.querySelector('link[rel="import"]')
 let content = link.import;
 let el = content.querySelector(".navbar");
+var x = document.getElementById("navbar-wrapper");
+const con = x.innerHTML;
+x.innerHTML = "<nav class='navbar'>" +
+   "<button onclick='toggleNavigation()' id='hamburger'><img src=''../IMG/hamburger_icon.png'></button>" +
+    "<ul id='navContent'>" +
+        "<li id='logo'>" +
+            "<a href='../index.html'>Diavola Calabrese</a>" +
+        "</li>" +
+        "<li class='rightnav'>" +
+            "<a href='../Pages/contact.html'>KONTAKT</a>" +
+      " </li>" +
+        "<li class='rightnav'>" +
+            "<a href='../Pages/cooking.html'>TILBEREDNING</a>" +
+        "</li>" +
+        "<li class='rightnav'>" +
+            "<a href='../Pages/reviews.html'>OMTALER</a>" +
+        "</li>" +
+        "<li class='rightnav'>" +
+            "<a href='../Pages/about.html'>OM PIZZAEN</a>"+
+        "</li>" +
+        "<li class='rightnav'>" +
+            "<a href='../index.html'>HJEM</a>" +
+        "</li>" +
+    "</ul>" + "</nav>"
 
 >>>>>>> dev
 document.body.appendChild(el.cloneNode(true));
